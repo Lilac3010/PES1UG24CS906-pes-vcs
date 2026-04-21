9facf6c8cc702b69b96f18483cdaac85594fd674
Advika Raj <advikaraj@gmail.com>
2026-04-21 09:24:20 -0400

    Phase 1: implement SHA-256 hashing for objects

--- object.c
+++ object.c
@@ -117,7 +117,7 @@ buffer[header_len] = '\0';
 memcpy(buffer + header_len + 1, data, len);
 
 /* TEMP: fake hash (we'll fix next commit) */
-memset(id_out->hash, 0, 32);
+compute_hash(buffer, total_len, id_out);
 
 /* free buffer */
 free(buffer);
