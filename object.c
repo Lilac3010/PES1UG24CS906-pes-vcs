8d7d359250a98d2f9bf41e99bd468b4a838dacad
Advika Raj <advikaraj@gmail.com>
2026-04-21 09:16:27 -0400

    Phase 1: add basic skeleton returning success

--- object.c
+++ object.c
@@ -96,7 +96,7 @@ int object_exists(const ObjectID *id) {
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
     // TODO: Implement
     (void)type; (void)data; (void)len; (void)id_out;
-    return -1;
+    return 0;
 }
 
 // Read an object from the store.
@@ -124,5 +124,5 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
 int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_t *len_out) {
     // TODO: Implement
     (void)id; (void)type_out; (void)data_out; (void)len_out;
-    return -1;
+    return 0;
 }
